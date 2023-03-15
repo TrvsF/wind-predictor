d268cd406bfdbc1bb3b033d3ae19ac6b5e3288d5
TrvsF <traviseff@gmail.com>
2023-03-15 21:56:07 +0000

    [a] data collector

--- src/tests/windthing.py
+++ src/tests/windthing.py
@@ -1,6 +1,7 @@
 from machine import Pin, ADC, Timer, RTC
 import time
 
+filename = ""
 rtc     = RTC()
 timer   = Timer(0)
 pot     = ADC(Pin(4))
@@ -8,6 +9,9 @@ pot.atten(ADC.ATTN_11DB)
 
 def start():
     global timer
+    global filename
+    date = rtc.datetime()
+    filename = f"{date[4]}-{date[5]}-{date[6]}.txt"
 
     timer.init(period=500, mode=Timer.PERIODIC, callback=printspeed)
 
@@ -19,6 +23,7 @@ def stop():
 def printspeed(args):
     global pot
     global rtc
+    global filename
 
     # get time & assign to xtime
     localtime = rtc.datetime()
@@ -26,7 +31,10 @@ def printspeed(args):
     mins = localtime[5]
     secs = localtime[6]
     xtime = (hour, mins, secs)
+    voltage = pot.read()
 
+    with open(f"{filename}", "a", encoding="utf-8") as file:
+        file.write(f"{voltage}:{xtime}\n")
     print(pot.read())
     print(xtime)
 

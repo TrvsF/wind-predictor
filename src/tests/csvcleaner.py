c4c70a759812be6daa64edd7948204e599a8186b
trvsf <traviseff@gmail.com>
2023-05-16 14:21:47 +0100

    [a] data visualistaion

--- src/tests/csvcleaner.py
+++ src/tests/csvcleaner.py
@@ -1,8 +1,12 @@
 import csv
+import matplotlib.pyplot as plt
+import statistics
 
 reader  = csv.DictReader(open('windraw.csv', 'r', encoding='utf-8'), delimiter=',', quoting=csv.QUOTE_NONE)
 newlist = []
 windq   = []
+datavis = []
+days    = 0
 counter = 0
 for item in reader:
     counter += 1
@@ -18,9 +22,14 @@ for item in reader:
         newlist.append((dt, windq[0], windq[1], windq[2], ws))
         # newlist.append((windq[0], windq[1], windq[2], ws))
 
-    windq.append(ws)
+    windq.append(float(ws))
 
-# print(newlist)
+    if dt == "0000":
+        datavis.append((days, statistics.mean(windq)))
+        days += 1
+        windq.clear()
+
+print(f"days counted : {len(datavis)}")
 
 with open("windspeeds.csv", "w", newline="", encoding="utf-8") as out:
     writer = csv.writer(out)
@@ -28,3 +37,8 @@ with open("windspeeds.csv", "w", newline="", encoding="utf-8") as out:
     # writer.writerow(["oldspeed1", "oldspeed2", "oldspeed3", "currentspeed"])
     for row in newlist:
         writer.writerow(row)
+
+plt.plot(*zip(*datavis[:365]))
+plt.xlabel("day")
+plt.ylabel("windspeed (m/s)")
+plt.show()
